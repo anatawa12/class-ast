f79b0fb95cfa1d740af301cd417fae594cf21990
anatawa12 <anatawa12@icloud.com>
2021-08-22 21:54:02 +0900

    add doc for each AttributeTarget

--- tree/src/main/java/com/anatawa12/classAst/attributes/AttributeTarget.kt
+++ tree/src/main/java/com/anatawa12/classAst/attributes/AttributeTarget.kt
@@ -12,12 +12,24 @@ class AttributeTarget(private val viewName: String) {
          */
         @JvmField
         val ClassFile = AttributeTarget("ClassFile")
+        /**
+         * Indicates the [field_info structure](spec:4.5)
+         */
         @JvmField
         val FieldInfo = AttributeTarget("FieldInfo")
+        /**
+         * Indicates the [method_info structure](spec:4.6)
+         */
         @JvmField
         val MethodInfo = AttributeTarget("MethodInfo")
+        /**
+         * Indicates the [Code_attribute structure](spec:4.7.3)
+         */
         @JvmField
         val Code = AttributeTarget("Code")
+        /**
+         * Indicates the [record_component_info structure](spec:4.7.30)
+         */
         @JvmField
         val RecordComponentInfo = AttributeTarget("RecordComponentInfo")
     }
