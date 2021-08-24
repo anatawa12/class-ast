0389e0b8248d11d4956ed52eeec0728e560e8bdf
anatawa12 <anatawa12@icloud.com>
2021-08-24 13:20:39 +0900

    fix Constant

--- tree/src/main/java/com/anatawa12/classAst/Constant.kt
+++ tree/src/main/java/com/anatawa12/classAst/Constant.kt
@@ -20,6 +20,10 @@ import com.anatawa12.autoVisitor.HasVisitor
     acceptName = "accept",
     subclasses = [
         Constant::class,
+        ConstantRef::class,
+        ConstantAnyMethodRef::class,
+        LoadableConstant::class,
+        ConstantValue::class,
         ConstantUtf8::class,
         ConstantInteger::class,
         ConstantFloat::class,
@@ -55,45 +59,45 @@ sealed class LoadableConstant(tag: UByte, sinceVersion: ClassFileVersion) : Cons
 sealed class ConstantValue(tag: UByte, sinceVersion: ClassFileVersion) : LoadableConstant(tag, sinceVersion)
 
 data class ConstantUtf8(val value: String) :
-    Constant(1u, ClassFileVersion(45u, 3u))
+    Constant(1u, CV45_3)
 
 data class ConstantInteger(val value: Int) :
-    ConstantValue(3u, ClassFileVersion(45u, 3u))
+    ConstantValue(3u, CV45_3)
 
 data class ConstantFloat(val value: Int) :
-    ConstantValue(4u, ClassFileVersion(45u, 3u)) {
+    ConstantValue(4u, CV45_3) {
     constructor(value: Float) : this(java.lang.Float.floatToRawIntBits(value))
 }
 
 data class ConstantLong(val value: Long) :
-    ConstantValue(5u, ClassFileVersion(45u, 3u))
+    ConstantValue(5u, CV45_3)
 
 data class ConstantDouble(val value: Long) :
-    ConstantValue(6u, ClassFileVersion(45u, 3u)) {
+    ConstantValue(6u, CV45_3) {
     constructor(value: Double) : this(java.lang.Double.doubleToRawLongBits(value))
 }
 
 data class ConstantClass(val name: ConstantUtf8) :
-    Constant(7u, ClassFileVersion(45u, 3u))
+    Constant(7u, CV45_3)
 
 data class ConstantString(val string: ConstantUtf8) :
-    ConstantValue(8u, ClassFileVersion(45u, 3u))
+    ConstantValue(8u, CV45_3)
 
 data class ConstantFieldRef(val owner: ConstantClass, val nameAndType: ConstantNameAndType) :
-    ConstantRef(9u, ClassFileVersion(45u, 3u))
+    ConstantRef(9u, CV45_3)
 
 data class ConstantMethodRef(val owner: ConstantClass, val nameAndType: ConstantNameAndType) :
-    ConstantAnyMethodRef(10u, ClassFileVersion(45u, 3u))
+    ConstantAnyMethodRef(10u, CV45_3)
 
 data class ConstantInterfaceMethodRef(val owner: ConstantClass, val nameAndType: ConstantNameAndType) :
-    ConstantAnyMethodRef(11u, ClassFileVersion(45u, 3u))
+    ConstantAnyMethodRef(11u, CV45_3)
 
 data class ConstantNameAndType(val name: ConstantUtf8, val descriptor: ConstantUtf8) :
-    Constant(12u, ClassFileVersion(45u, 3u))
+    Constant(12u, CV45_3)
 
 @AutoToString("kind", "reference")
 class ConstantMethodHandle private constructor(val kind: Kind, val reference: ConstantRef) :
-    LoadableConstant(15u, ClassFileVersion(51u, 0u)) {
+    LoadableConstant(15u, CV51_0) {
     enum class Kind(val id: UByte) {
         GetField(1u),
         GetStatic(2u),
@@ -138,20 +142,20 @@ class ConstantMethodHandle private constructor(val kind: Kind, val reference: Co
 }
 
 data class ConstantMethodType(val descriptor: ConstantUtf8) :
-    LoadableConstant(16u, ClassFileVersion(51u, 0u))
+    LoadableConstant(16u, CV51_0)
 
 // TODO: make bootstrapMethod reference
 data class ConstantDynamic(val bootstrapMethod: Int, val nameAndType: ConstantNameAndType) :
-    LoadableConstant(17u, ClassFileVersion(55u, 0u))
+    LoadableConstant(17u, CV55_0)
 
 data class ConstantInvokeDynamic(val bootstrapMethod: Int, val nameAndType: ConstantNameAndType) :
-    Constant(18u, ClassFileVersion(51u, 0u))
+    Constant(18u, CV51_0)
 
 data class ConstantModule(val name: ConstantUtf8) :
-    Constant(19u, ClassFileVersion(53u, 0u))
+    Constant(19u, CV53_0)
 
 data class ConstantPackage(val name: ConstantUtf8) :
-    Constant(20u, ClassFileVersion(53u, 0u))
+    Constant(20u, CV53_0)
 
 @GenerateVisitor(Constant::class)
 abstract class ConstantVisitor<R>
